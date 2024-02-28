e92831d3c10a616be0e5148340e691d39fb71f6f
milsyy <128106812+milsyy@users.noreply.github.com>
2024-02-28 03:08:02 -0500

    Finished the clock animation

--- script.js
+++ script.js
@@ -21,11 +21,84 @@ const clock = () => {
   ctx.lineWidth = 14;
   ctx.strokeStyle = "#800000";
   ctx.arc(0, 0, 142, 0, Math.PI * 2, true);
-  ctx.stroke();
+  ctx.stroke(); // outline appears thinner due to the fill being on top of the stroke, call fill first to avoid this
   ctx.fill();
   ctx.restore();
 
+  // Draw hour lines
+  ctx.save();
+  for (let i = 0; i < 12; i++) {
+    ctx.beginPath();
+    ctx.rotate(Math.PI / 6);
+    ctx.moveTo(105, 0);
+    ctx.lineTo(125, 0);
+    ctx.stroke();
+  }
+  ctx.restore();
+
+  // Draw minute lines
+  ctx.save();
+  for (let i = 0; i < 60; i++) {
+    if (i % 5 !== 0) {
+      ctx.beginPath();
+      ctx.moveTo(122, 0);
+      ctx.lineTo(125, 0);
+      ctx.stroke();
+    }
+    ctx.rotate(Math.PI / 30);
+  }
+  ctx.restore();
+
+  // Get current time
+  const hr = now.getHours() % 12;
+  const min = now.getMinutes();
+  const sec = now.getSeconds();
+
+  // Draw hour hand
+  ctx.save();
+
+  ctx.beginPath();
+  ctx.rotate((Math.PI / 6) * hr + (Math.PI / 360) * min) +
+    (Math.PI / 21600) * sec;
+  ctx.strokeStyle = "#800000";
+  ctx.lineWidth = 12;
+  ctx.moveTo(-20, 0);
+  ctx.lineTo(75, 0);
+  ctx.stroke();
+
+  ctx.restore();
+
+  // Draw minute hand
+  ctx.save();
+
+  ctx.beginPath();
+  ctx.rotate((Math.PI / 30) * min) + (Math.PI / 360) * sec;
+  ctx.strokeStyle = "#800000";
+  ctx.lineWidth = 12;
+  ctx.moveTo(-20, 0);
+  ctx.lineTo(113, 0);
+  ctx.stroke();
+
+  ctx.restore();
+
+  // Draw second hand
+  ctx.save();
+
+  ctx.beginPath();
+  ctx.fillStyle = "#f56f3b";
+  ctx.arc(0, 0, 7, 0, Math.PI * 2, true);
+  ctx.fill();
+  ctx.rotate((Math.PI / 30) * sec);
+  ctx.strokeStyle = "#f56f3b";
+  ctx.lineWidth = 6;
+  ctx.moveTo(-20, 0);
+  ctx.lineTo(115, 0);
+  ctx.stroke();
+  ctx.restore();
+
   ctx.restore(); // Restore default state
+
+  requestAnimationFrame(clock);
 };
 
-clock();
+requestAnimationFrame(clock);
